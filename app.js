ce025d78487750f7b34002ab9f0a974b316485f0
ccaldarella99 <christopher.caldarella@outlook.com>
2017-03-19 13:30:45 -0600

    photo resize
    Renamed so photos that were not showing correctly because they were not
    referenced in a case-sensitive manner.
    Resized photos on the bottom to display in the overlay when selected.

--- app.js
+++ app.js
@@ -1,32 +1,79 @@
-metProposal(1);
+var thisSlide = 0;
+var picSlide = 0;
+metProposal(thisSlide);
+slideShow(picSlide);
+
+document.onkeydown = function(evt) {
+	evt = evt || window.event;
+	var isEscape = false;
+	if ("key" in evt) {
+		isEscape = (evt.key == "Escape" || evt.key == "Esc");
+	} else {
+		isEscape = (evt.keyCode == 27);
+	}
+	if (isEscape) {
+		closeOverlay('lodging', 'lodging-contents');
+		closeOverlay('slideshow', 'slideshow-contents');
+	}
+};
+
+function slideShow(n) {
+	var pSlides = document.getElementsByClassName("slide-img");
+	var maxSlides = pSlides.length;
+	
+	picSlide += n;
+	if (picSlide >= maxSlides) {
+		picSlide = 0;
+	} else if (picSlide < 0) {
+		picSlide = maxSlides-1;
+	}
+
+	//Hide All Slides
+	for (var i = 0; i < maxSlides; i++) {
+		pSlides[i].style.display = "none";
+//		pSlides[i].className = pSlides[i].className.replace(" active","");
+	}
+	
+	pSlides[picSlide].style.display = "inline-block";
+//	pSlides[picSlides].className += " active";
+}
 
 function metProposal(n) {
 	var mpSlides = document.getElementsByClassName("text-slides");
-	var arrows = document.getElementsByClassName("arrow-btn");
+	var maxSlides = mpSlides.length;
 	
-	if (n==1) {
-		mpSlides[0].style.display = "block";
-		mpSlides[0].className += " active";
-		arrows[0].className += " active";
-		mpSlides[1].style.display = "none";
-		mpSlides[1].className = mpSlides[1].className.replace(" active","");
-		arrows[1].className = arrows[1].className.replace(" active","");
-	} else {
-		mpSlides[1].style.display = "block";
-		mpSlides[1].className += " active";
-		arrows[1].className += " active";
-		mpSlides[0].style.display = "none";
-		mpSlides[0].className = mpSlides[0].className.replace(" active","");
-		arrows[0].className = arrows[0].className.replace(" active","");
+	thisSlide += n;
+	if (thisSlide >= maxSlides) {
+		thisSlide = 0;
+	} else if (thisSlide < 0) {
+		thisSlide = maxSlides-1;
 	}
+
+	//Hide All Slides
+	for (var i = 0; i < maxSlides; i++) {
+		mpSlides[i].style.display = "none";
+		mpSlides[i].className = mpSlides[i].className.replace(" active","");
+	}
+	
+	mpSlides[thisSlide].style.display = "block";
+	mpSlides[thisSlide].className += " active";
+	
+//	document.getElementById("showThis").innerHTML = thisSlide + "/" + maxSlides;
+
 }
 
-function openOverlay(item, content) {
-	document.getElementById(content).style.display = "block";
+function openOverlay(item, content, n) {
+	if (n>=0) {
+		picSlide = 0;
+		slideShow(n);
+	}
+	document.getElementById(content).style.display = "inline-block";
 	document.getElementById(item).style.width = "100%";
+	document.getElementById("escape").style.display = "block";
 }
 
 function closeOverlay(item, content) {
 	document.getElementById(content).style.display = "none";
 	document.getElementById(item).style.width = "0%";
+	document.getElementById("escape").style.display = "none";
 }
